1db81d6ee71632b3822f3f917d1369347c473a98
aj1mm <avaijimmerson@gmail.com>
2018-07-09 23:20:31 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -3,4 +3,4 @@ puts "Plant legs far apart, bend knees slightly and keep posture loose"
 puts "Lift right foot"
 puts"Return right foot to the ground"
 puts "Finishing with a small skip-step backward"
-puts 
+puts ""
